a2ed7095d9617446aac0bcebcdf25a9d3d9829a5
sandi-devops <sandi.devops@gmail.com>
2024-05-20 17:47:01 +0900

    fist time upload

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Portfolio</title>
+    
+    <link rel="stylesheet" href="/css/app.css">
+    
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
+
+
+
---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>IT me</h1>
+@endsection
