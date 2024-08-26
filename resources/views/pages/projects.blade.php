cb1af8de6ac59409d2f6ca5638ab3de54c027fc9
sandi-devops <sandi.devops@gmail.com>
2024-08-27 01:26:57 +0900

    project ui change

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -9,9 +9,9 @@
         <!-- Add your project details here -->
         <div class="col-md-4">
             <div class="card">
-                <img src="project1.jpg" class="card-img-top" alt="Project 1">
+               
                 <div class="card-body">
-                    <h5 class="card-title">Project 1</h5>
+                    <h5 class="card-title">JOB Project</h5>
                     <p class="card-text">Brief description of Project 1.</p>
                 </div>
             </div>
